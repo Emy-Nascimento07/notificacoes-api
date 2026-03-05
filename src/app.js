6464550fbdaf1049678c89f88ff4e93d20ba3add
Emilly <emilly.r.nascimento6@aluno.senai.br>
2026-03-05 09:08:04 -0300

    Atualizar "app.js" para incluir rotas e atualizar mensagem da rota raiz

--- src/app.js
+++ src/app.js
@@ -4,9 +4,20 @@ const app = express();
 // Middleware para ler JSON no body das requisições
 app.use(express.json());
 
-// Rota de teste - vamos remover depois
+// Importar rotas
+const eventoRoutes = require("./routes/eventoRoutes");
+
+// Usar rotas com prefixo
+app.use("/eventos", eventoRoutes);
+
+// Rota raiz (informativa)
 app.get("/", (req, res) => {
-    res.json({ mensagem: "API de notificações funcionando! 🚀"})
+    res.json({
+        mensagem: "API de Notificações",
+        rotas: {
+            eventos: "/eventos"
+        },
+    });
 });
 
 module.exports = app;
